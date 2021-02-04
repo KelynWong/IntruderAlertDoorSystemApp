a031a5c371102d8a4d8e3e0c3e8c752889624424
Kelyn Wong <kelynwonget@gmail.com>
2021-02-04 21:43:37 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -4,100 +4,79 @@ import pandas as pd
 import time
 import requests
 import json
+import datetime
+import _strptime
+
+dateList = []
+numberList = []
+
+dates = []
+noOfEnter = []
+
+count = 0
+totalOfThatDay = 0
+
+resp=requests.get("https://api.thingspeak.com/channels/1230188/fields/1.json") #to read only field 2, 10 values
 
 
-ids = []
-lockUnlock = []
-offOn = []
-
-
-resp=requests.get("https://api.thingspeak.com/channels/1091956/feeds.json?results=10") #read all fields, 10 values
-#resp=requests.get("https://api.thingspeak.com/channels/645078/fields/2.json?results=10") #to read only field 2, 10 values
-
-print(resp.text)
 results=json.loads(resp.text) #convert json into Python object
+feeds = results["feeds"]
 
-for x in range(10):
-    ids.append(results["feeds"][x]["field1"])
-    lockUnlock.append(results["feeds"][x]["field2"])
-    offOn.append(results["feeds"][x]["field3"])
-    print("Downloaded sample",x,": id =",results["feeds"][x]["field1"],", lockUnlock =",results["feeds"][x]["field2"],", offOn =",results["feeds"][x]["field3"])
+for x in range(len(feeds)):
+    if feeds[x]["field1"] != None:
+        date_time_obj = datetime.datetime.strptime(feeds[x]["created_at"], '%Y-%m-%dT%H:%M:%SZ')
+        dateList.append(date_time_obj.date())
+        numberList.append(int(results["feeds"][x]["field1"]))
 
-
+for x in range(len(numberList)):
+    temp = dateList[x]
+    if x + 1 != len(numberList):
+        if dateList[x + 1] == temp:
+            totalOfThatDay = totalOfThatDay + 1
+            if x == 1:
+                totalOfThatDay = totalOfThatDay + 1
+        else:
+            dates.append(temp.strftime('%d/%m/%Y'))
+            noOfEnter.append(totalOfThatDay)
+            totalOfThatDay = 0
+        temp = dateList[x + 1]
+    else:
+        totalOfThatDay = totalOfThatDay + 1
+        dates.append(temp.strftime('%d/%m/%Y'))
+        noOfEnter.append(totalOfThatDay)
 
 st.title('Intruder alert door system')
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': ids,
-    'second column': lockUnlock,
-    'third column': offOn
-}))
-
-
-
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
-
-
-
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(map_data)
-
-
-
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randn(20, 3),
-       columns=['a', 'b', 'c'])
-
-    st.line_chart(chart_data)
-
-
-
 df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
+  'date': dates,
+  'noOfEnter': noOfEnter
 })
 
-option1 = st.selectbox(
-    'Which number do you like best?',
-     df['first column'])
+df = df.rename(columns={'date':'index'}).set_index('index')
 
-'You selected: ', option1
+df
 
-
-
-#option2 = st.sidebar.selectbox(
-#    'Which number do you like best?',
-#     df['first column'])
-
-#'You selected:', option2
+st.line_chart(df)
 
 
+buzzerOnButton = st.button('On buzzer');
+buzzerOffButton = st.button('Off buzzer');
+doorUnlockButton = st.button('Unlock door');
+doorLockButton = st.button('Lock door');
 
-left_column, right_column = st.beta_columns(2)
-pressed = left_column.button('Press me?')
-if pressed:
-    right_column.write("Woohoo!")
+if buzzerOnButton:
+    st.write('The buzzer is currently on')
+    requests.get("https://api.thingspeak.com/update?api_key=Q539CRA8JC5EWP86&field3=1")
 
-expander = st.beta_expander("FAQ")
-expander.write("Here you could put in some really, really long explanations...")
+if buzzerOffButton:
+    st.write('The buzzer is currently off')
+    requests.get("https://api.thingspeak.com/update?api_key=Q539CRA8JC5EWP86&field3=0")
 
+if doorUnlockButton:
+    st.write('The door is currently unlocked')
+    requests.get("https://api.thingspeak.com/update?api_key=Q539CRA8JC5EWP86&field2=1")
 
+if doorLockButton:
+    st.write('The door is currently locked')
+    requests.get("https://api.thingspeak.com/update?api_key=Q539CRA8JC5EWP86&field2=0")
 
-# Add a placeholder
-latest_iteration = st.empty()
-bar = st.progress(0)
-
-for i in range(100):
-  # Update the progress bar with each iteration.
-  latest_iteration.text('hi')
-  bar.progress(i + 1)
-  time.sleep(0.1)
