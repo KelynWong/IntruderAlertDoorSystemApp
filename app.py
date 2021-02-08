94e88eb2f781c7311f64e32ac428f4c922bce90d
Kelyn Wong <kelynwonget@gmail.com>
2021-02-08 14:51:47 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ for x in range(len(numberList)):
 st.title('Intruder alert door system')
 st.header('Live video')
 st.markdown("""
-<iframe width="560" height="315" src="https://www.youtube.com/embed/VRpXLf4lW9U" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/oXlZAWP-qcw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 """, unsafe_allow_html=True)
 
 st.header('Analytics')
